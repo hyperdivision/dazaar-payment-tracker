d22e9a6c71cfdb6c51f38667bf12e8a90a5e6acf
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-11-10 13:12:34 +0100

    add bigint support for eth payments

---
+++ bigint.js
@@ -0,0 +1,71 @@
+module.exports = function (perSecond, minSeconds = 0n, offset = 0n) {
+  if (perSecond === undefined) throw new Error('rate must be specified')
+
+  const payments = []
+
+  function add (payment) {
+    if (payment.amount === undefined || payment.time === undefined) {
+      throw new Error('payment must be of format { amount: x, time: y }')
+    }
+    payments.push(payment)
+  }
+
+  function active () {
+    if (!perSecond) return true
+    return remainingFunds() > 0n
+  }
+
+  function remainingTime () {
+    const funds = remainingFunds()
+    console.log('funds', funds)
+    return max(0, (funds * 1000n) / perSecond)
+  }
+
+  function remainingFunds () {
+    let now = BigInt(Date.now()) + minSeconds * 1000n
+    now -= offset // compensate delay for the seller to receive block
+
+    let funds = 0n
+
+    for (let i = 0; i < payments.length; i++) {
+      const { amount, time } = payments[i]
+      console.log(i + 1 < payments.length)
+      const nextTime = i + 1 < payments.length ? payments[i + 1].time : now
+
+      // add current payment
+      funds += amount 
+
+      // subtract amount spent since previous payment
+      console.log(perSecond, nextTime, time)
+      console.log(nextTime - time)
+      const consumed = max(0n, perSecond * (nextTime - time) / 1000n)
+      funds -= consumed
+
+      // if funds are out, clear all earlier payments
+      if (funds < 0) {
+        payments.splice(0, i + 1)
+        i = -1
+        funds = 0
+      }
+    }
+
+    return funds
+  }
+
+  function gc () {
+    remainingFunds()
+    return payments.length
+  }
+
+  return {
+    add,
+    active,
+    remainingTime,
+    remainingFunds,
+    gc
+  }
+}
+
+function max (a, b) {
+  return (a >= b) ? a : b
+}
